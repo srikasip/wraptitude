5be13ac80d70bd33a04150797cab0d22b73952b6
Srikar Prasad <srikasip@gmail.com>
2018-03-12 21:43:44 -0400

    Got some smoothe scrolling.

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  console.log("This is home.js...I am loaded");
+  SmoothScrollLinks();
 });
+
+
+function SmoothScrollLinks()
+{
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    var windowHeight = $(window).outerHeight();
+    if(target.length) {
+      event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
+}
